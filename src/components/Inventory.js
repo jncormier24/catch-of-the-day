8807927ec6e34549fcdf8084a4c1cd9e5bf2c8a3
Joseph Cormier <jncormier24@gmail.com>
2018-03-02 14:39:40 -0500

    added a state function for adding fish

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
+import AddFishForm from './AddFishForm'
 
 export class Inventory extends Component {
   render () {
-    return <div className='inventory'>Inventory!!!</div>
+    return (
+      <div className='inventory'>
+        Inventory!!!
+        <AddFishForm addFish={this.props.addFish} />
+      </div>
+    )
   }
 }
 
