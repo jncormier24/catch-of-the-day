c6076cdef1339f8f13ff35c8c5ac2428b30cf2ee
Joseph Cormier <jncormier24@gmail.com>
2018-03-02 12:17:06 -0500

    adding ref for store picker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+  storeName = React.createRef()
+
+  goToStore = (e) => {
+    e.preventDefault()
+
+    console.log(this.storeName.value.value)
+  }
   render () {
     return (
-      <form action='' className='store-selector'>
+      <form action='' className='store-selector' onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type='text' placeholder='Store Name' required />
+        <input
+          type='text'
+          placeholder='Store Name'
+          defaultValue={getFunName()}
+          ref={this.storeName}
+          required />
         <button type='submit'>Visit Store</button>
       </form>
     )
