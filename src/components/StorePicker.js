2a09e6e286bd7eac0bd22e321e8c584ec92e8ff7
Joseph Cormier <jncormier24@gmail.com>
2018-03-02 09:36:05 -0500

    added style; created form for store name

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 class StorePicker extends React.Component {
   render () {
-    return <p>hello</p>
+    return (
+      <form action='' className='store-selector'>
+        <h2>Please Enter A Store</h2>
+        <input type='text' placeholder='Store Name' required />
+        <button type='submit'>Visit Store</button>
+      </form>
+    )
   }
 }
 
